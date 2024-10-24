c827c7629fcda7cfd0405966b1c0c6e70ae46226
moaztobok <moaztobok@gmail.com>
2024-10-24 15:15:51 +0100

    changing middleware and language settings

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,34 +5,18 @@ import { locales } from './i18n';
 
 // Create the middleware configuration
 const intlMiddleware = createMiddleware({
-  // Define your locales
   locales: locales,
-
-  // Set your default locale
-  defaultLocale: 'ar',
-
-  // Optional: Customize localePrefix to control URL structure
-  localePrefix: 'as-needed', // 'as-needed' | 'never' | 'always'
-
-  // Optional: Add locale detection
+  defaultLocale: 'en',
+  localePrefix: 'always', // 'as-needed' | 'never' | 'always'
   localeDetection: true
+
 });
 
-// Export the middleware handler
 export default function middleware(request: NextRequest) {
-  // Optional: You can add custom logic here before calling intlMiddleware
-  // For example, checking cookies, headers, or user preferences
 
   return intlMiddleware(request);
 }
 
-// Configure the middleware matcher
 export const config = {
-  // Match all pathnames except for
-  // - /api (API routes)
-  // - /_next (Next.js internals)
-  // - /_vercel (Vercel internals)
-  // - /images (e.g. public files)
-  // - /favicon.ico (favicon file)
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)", "/(en|ar|fr)/:path*"]
 };
